599aec06ed56d32641337f6b6936de0a80619bd7
Lucas Martins <lucasf.martins94@outlook.com>
2026-07-12 20:21:58 -0300

    Initial Spring Boot project

---
+++ src/test/java/com/cardex/api/CardexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardex.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
